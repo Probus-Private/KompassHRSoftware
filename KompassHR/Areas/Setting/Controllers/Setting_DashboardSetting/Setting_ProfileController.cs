95cf10e27ce759415588f20bdcf7c2a1336332ea
Akash Laptop <Akashkumbhar89@gmail.com>
2026-04-07 16:39:52 +0530

    updated

---
+++ KompassHR/Areas/Setting/Controllers/Setting_DashboardSetting/Setting_ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KompassHR.Areas.Setting.Controllers
+{
+    public class Setting_ProfileController : Controller
+    {
+        // GET: Setting/Setting_Profile
+        public ActionResult Setting_Profile()
+        {
+            return View();
+        }
+    }
+    public class Setting_SecurityController : Controller
+    {
+        // GET: Setting/Setting_Security
+        public ActionResult Setting_Security()
+        {
+            return View();
+        }
+    }
+}
